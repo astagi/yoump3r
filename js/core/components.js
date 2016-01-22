ff4b2437fd92738a1210bdc40a717dab8467e652
Andrea Stagi <stagi.andrea@gmail.com>
2016-01-22 20:37:36 +0100

    Avoid executing multiple requests at the same time

--- js/core/components.js
+++ js/core/components.js
@@ -7,13 +7,15 @@ angular.module('yoump3r.components', ['yoump3r.services.client'])
       onDeleteSong: '&'
     },
     templateUrl: '/static/partials/songrow.html',
-    controller: ['$scope', '$element', function ( $scope, $element ) {
+    controller: ['$scope', '$element', '$timeout', '$q', function ( $scope, $element, $timeout, $q ) {
       $scope.selectedSong = {};
       $scope.suggestedSongs = [];
       $scope.song = '';
       $scope.artist = '';
       $scope.title = '';
       $scope.downloadMp3Url = '';
+      $scope.qTimeout;
+      $scope.qHttp;
       $scope.downloadSong = function () {
         $scope.downloadMp3Url = 'http://www.youtubeinmp3.com/download/?video=https://www.youtube.com/watch?v=' + $scope.selectedSong.id + '&autostart=1';
       };
@@ -22,14 +24,23 @@ angular.module('yoump3r.components', ['yoump3r.services.client'])
       };
       $scope.searchSong = function () {
         $scope.title = $scope.artist + ' - ' + $scope.song;
-        yoump3rclient.searchSong($scope.title)
-        .success(function (songs) {
-          console.log(songs);
-          $scope.suggestedSongs = songs;
-        })
-        .error(function (error) {
-
-        });
+        if ( $scope.qTimeout ) {
+          $timeout.cancel($scope.qTimeout);
+        }
+        if ( $scope.qHttp ) {
+          $scope.qHttp.resolve();
+        }
+        $scope.qHttp = $q.defer();
+        $scope.qTimeout = $timeout(function () {
+          yoump3rclient.searchSong($scope.title, $scope.qHttp)
+          .success(function (songs) {
+            console.log(songs);
+            $scope.suggestedSongs = songs;
+          })
+          .error(function (error) {
+            console.log(error);
+          });
+        }, 200);
       };
     }]
   };
--- js/core/services.js
+++ js/core/services.js
@@ -3,8 +3,11 @@ angular.module('yoump3r.services.client', [])
 
     var urlBase = '/api/v1';
 
-    this.searchSong = function (query) {
-      return $http.get(urlBase + '/songs/search/?q=' + query);
+    this.searchSong = function (query, canceler) {
+      return $http.get(
+        urlBase + '/songs/search/?q=' + query,
+        {timeout: canceler.promise}
+      );
     };
 
   }]);
